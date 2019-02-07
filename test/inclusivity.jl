93e2c51a8f0e09ebb8d88eb29e9e8ff945fd0fc1
Curtis Vogt <curtis.vogt@gmail.com>
2019-02-07 11:44:10 -0600

    Update test/inclusivity.jl
    Co-Authored-By: morris25 <sam.morrison@invenia.ca>

--- test/inclusivity.jl
+++ test/inclusivity.jl
@@ -49,7 +49,7 @@
         @test hash(inc) != hash(diff)
 
         # Verify that Inclusivity is treated as a scalar during broadcast
-        @test Inclusivity(false, true) .== Inclusivity(false, true)
+        @test size(Inclusivity(false, true) .== Inclusivity(false, true)) == ()
     end
 
     @testset "display" begin

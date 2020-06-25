84ae027f31727ae253b6d48c316ee169d0077b55
Curtis Vogt <curtis.vogt@gmail.com>
2020-06-25 08:01:29 -0500

    Control tests includes via env variable TESTS

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,13 +7,31 @@ using TimeZones
 
 const BOUND_PERMUTATIONS = product((Closed, Open), (Closed, Open))
 
-@testset "Intervals" begin
-    include("inclusivity.jl")
-    include("endpoint.jl")
-    include("interval.jl")
-    include("anchoredinterval.jl")
-    include("comparisons.jl")
-    include("plotting.jl")
+const ALL_TESTS = (
+    :inclusivity,
+    :endpoint,
+    :interval,
+    :anchoredinterval,
+    :comparisons,
+    :plotting,
+    :doctest,
+)
 
-    doctest(Intervals)
+const TESTS = let
+    tests = Symbol.(split(get(ENV, "TESTS", ""), r"\s+", keepempty=false))
+    if isempty(tests)
+        ALL_TESTS
+    else
+        tests
+    end
+end
+
+@testset "Intervals" begin
+    :inclusivity in TESTS && include("inclusivity.jl")
+    :endpoint in TESTS && include("endpoint.jl")
+    :interval in TESTS && include("interval.jl")
+    :anchoredinterval in TESTS && include("anchoredinterval.jl")
+    :comparisons in TESTS && include("comparisons.jl")
+    :plotting in TESTS && include("plotting.jl")
+    :doctest in TESTS && doctest(Intervals)
 end

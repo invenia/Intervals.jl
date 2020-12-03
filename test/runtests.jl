f2badccc06fa9c591e8edddd287a24bc543a7b91
Fernando Chorney <fernando.chorney@invenia.ca>
2020-12-03 14:26:00 -0600

    Use other slack notifier, test notifications

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,6 +13,7 @@ const BOUND_PERMUTATIONS = product((Closed, Open), (Closed, Open))
 include("test_utils.jl")
 
 @testset "Intervals" begin
+    error()
     include("inclusivity.jl")
     include("endpoint.jl")
     include("interval.jl")

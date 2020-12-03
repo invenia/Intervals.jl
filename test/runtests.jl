038153fed505b63526aba1678d916ee087d9e043
Fernando Chorney <fernando.chorney@invenia.ca>
2020-12-03 14:47:40 -0600

    Revert CI test

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,7 +13,6 @@ const BOUND_PERMUTATIONS = product((Closed, Open), (Closed, Open))
 include("test_utils.jl")
 
 @testset "Intervals" begin
-    error()
     include("inclusivity.jl")
     include("endpoint.jl")
     include("interval.jl")

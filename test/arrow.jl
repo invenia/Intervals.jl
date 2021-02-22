556662df7e4d4fa8201bb28a3f27df405aff56b3
rofinn <rory.finnegan@gmail.com>
2021-02-22 11:51:25 -0600

    Address review comments.

--- test/arrow.jl
+++ test/arrow.jl
@@ -1,7 +1,7 @@
 @testset "arrow" begin
     zdt_start = ZonedDateTime(2016, 8, 11, 1, tz"America/Winnipeg")
     zdt_end = ZonedDateTime(2016, 8, 12, 0, tz"America/Winnipeg")
-    table = (time = HE.(zdt_start:Hour(1):zdt_end), val = rand(24))
+    table = (time = HE.(zdt_start:Hour(1):zdt_end), val = collect(1:24))
 
     # Just test that we can save and load our table type without the time column being
     # converted to a NamedTuple
@@ -9,7 +9,8 @@
         file = joinpath(d, "data.arrow")
         Arrow.write(file, table)
         loaded = Arrow.Table(file)
+        @test loaded isa Arrow.Table
         @test loaded.time == table.time
-        @test loaded.val ≈ table.val
+        @test loaded.val == table.val
     end
 end

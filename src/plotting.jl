a2e219cfb1fb58de7d9dbdb5e00508ca2de18311
Alex Robson <alex.robson@invenialabs.co.uk>
2021-04-20 15:14:38 +0100

    Fix plotting for Plots > v1.11

--- src/plotting.jl
+++ src/plotting.jl
@@ -5,24 +5,25 @@ interval_marker(x::Type{Open}) = :none
 
 # TODO: Add support for plotting unbounded intervals
 @recipe function f(xs::AbstractVector{<:AbstractInterval{T,L,R}}, ys) where {T, L <: Bounded, R <: Bounded}
-    new_xs = T[]
-    new_ys = []
+    new_xs = Vector{T}[]
+    new_ys = Vector{Any}[]
     markers = Symbol[]
     for (x, y) in zip(xs, ys)
-        # To cause line to not be connected, need to add a breaker point with
-        # NaN in one of the coordinates. We put that in `new_ys` as that is probably a
-        # float. where-as new_xs is potentially a DateTime etc that would not accept NaN
-        append!(new_xs, [first(x), last(x), last(x)])
-        append!(new_ys, [y, y, NaN])
-        append!(markers, interval_markers(x))
-        push!(markers, :none)
+        # To cause line to not be connected, need to divide individually separated segments into separate vectors
+        # This is as of v1.11 in Plots.
+        # See https://github.com/JuliaPlots/Plots.jl/blob/master/src/utils.jl#L104
+        append!(new_xs, [[first(x), last(x)]])
+        append!(new_ys, [[y, y]])
     end
-
+    append!(markers, interval_markers(first(xs)))
+    
     # Work around GR bug that shows :none as a marker
     # TODO: remove once https://github.com/jheinen/GR.jl/issues/295  is fixed
     markeralpha := [x == :none ? 0 : 1 for x in markers]
 
     markershape := markers
     seriestype  := :path  # always a path, even in a scatter plot
+
     new_xs, new_ys
+    
 end

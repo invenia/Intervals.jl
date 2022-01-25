e04f9d482168d43f6471dc64d7175d0965c2da5b
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2022-01-24 21:08:03 -0600

    Set minimum supported julia version to 1.6 (LTS) (#186)
    * ci: Update LTS version
    
    * ci: Remove 1.5 workflow
    
    * Adjust display tests
    
    * Adjust plotting tests tolerance
    
    * Set minimum supported julia version to 1.6

--- src/compat.jl
+++ src/compat.jl
@@ -21,11 +21,3 @@ function Serialization.deserialize(s::AbstractSerializer, ::Type{AnchoredInterva
 
     return AnchoredInterval{P,T,L,R}(anchor)
 end
-
-# Works around an issue where a `StepRangeLen` tries to convert the step to an
-# `AbstractInterval`.
-# Issue introduced in https://github.com/JuliaLang/julia/pull/23194 (a698230daf6)
-# and fixed in https://github.com/JuliaLang/julia/pull/34412 (fca037a162)
-if v"0.7.0-DEV.1399" <= VERSION < v"1.5.0-DEV.127"
-    Base.step(r::StepRangeLen{<:AbstractInterval}) = r.step
-end

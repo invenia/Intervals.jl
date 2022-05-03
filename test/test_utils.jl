e428f0611fda65e71dde83fd66ae51292edf64f1
Curtis Vogt <curtis.vogt@gmail.com>
2022-05-03 15:29:36 -0500

    Drop serialized headers below Julia 1.6

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -2,14 +2,8 @@
 # format is the same as the last version of Julia we've tested against.
 const SERIALIZED_HEADER = if VERSION >= v"1.5"
     "7JL\n\x04\0\0"
-elseif VERSION >= v"1.4"
-    "7JL\t\x04\0\0"
-elseif VERSION >= v"1.2"
-    "7JL\b\x04\0\0"
-elseif VERSION >= v"1.0"
-    "7JL\a\x04\0\0"
 else
-    error("Julia versions earlier than 1.0 are unsupported")
+    error("Julia versions earlier than 1.5 are unsupported")
 end
 
 # Declare a new `isnan` and `isinf` functions to avoid type piracy. These new function work
